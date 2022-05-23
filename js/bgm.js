57e2860d77b6b9b93f8e52b09e849bfab74bfc9e
w55587496 <w55587496@yahoo.com.tw>
2022-05-23 12:52:36 +0800

    add copy func

--- js/bgm.js
+++ js/bgm.js
@@ -16,6 +16,31 @@ var vm = new Vue({
         } 
       }
     });
+  },
+  // https://stackoverflow.com/questions/45041383/target-the-textarea-of-a-child-component-to-copy-to-clipboard
+  methods: {
+    copy(music){
+      // console.log(music.name)
+      var dummy = document.createElement("input");
+      // Add it to the document
+      
+      document.body.appendChild(dummy);
+      // Set its ID
+      dummy.setAttribute("id", "dummy_id");
+    
+      // Output the array into it
+      document.getElementById("dummy_id").value = music.name;
+    
+      // Select it
+      dummy.select();
+    
+      // Copy its contents
+      document.execCommand("copy");
+    
+      // Remove it as its not needed anymore
+      document.body.removeChild(dummy);
+    }
   }
 });
+
 
